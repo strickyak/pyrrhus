f389604c064a7e85accbafa8c73d336cd46c743d
central <central@think.yak.net>
2012-11-18 23:05:57 -0800

    runs-t3

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
-all: t1 t2
+all: t1 t2 t3
 
 t1 : t1.go
 	go build t1.go runtime.go
-t2 : t2.go
-	go build t2.go runtime.go
-
+	./t1
 t1.go : t1.py
 	python c.py t1.py > t1.go || { mv t1.go t1.bad ; false ; }
+
+t2 : t2.go
+	go build t2.go runtime.go
+	./t2
 t2.go : t2.py
 	python c.py t2.py > t2.go || { mv t2.go t2.bad ; false ; }
+
+t3 : t3.go
+	go build t3.go runtime.go
+	./t3
+t3.go : t3.py
+	python c.py t3.py > t3.go || { mv t3.go t3.bad ; false ; }
 
 clean:
 	rm -f t*[0-9].go t*[0-9] t*[0-9].bad
--- runtime.go
+++ runtime.go
@@ -1,26 +1,61 @@
 package main
-  import "fmt"
 
-  type Any interface {
-    func PrintableValue(a Any) string {
-      switch t := a.(type)
-      default:
-        panic("Type not handled in PrintableValue")
-      case int:
-        return fmt.SPrintf("%d", t)
-      case string:
-        return t
-    }
-  }
+import "fmt"
 
-  func CompareLtE(a Any, b Any) Any {
-    return a.(int) <= b.(int)
-  }
+type Any interface{}
 
-  func BinOpAdd(a Any, b Any) Any {
-    return a.(int) + b.(int)
-  }
+func PrintableValue(a Any) string {
+	switch t := a.(type) {
+	case bool:
+		return fmt.Sprintf("%v", t)
+	case int:
+		return fmt.Sprintf("%d", t)
+	case string:
+		return t
+	}
+	panic("Type not handled in PrintableValue")
+}
 
-  func BinOpSub(a Any, b Any) Any {
-    return a.(int) - b.(int)
-  }
+func CompareLt(a Any, b Any) Any {
+	return a.(int) < b.(int)
+}
+
+func CompareLtE(a Any, b Any) Any {
+	return a.(int) <= b.(int)
+}
+
+func CompareGt(a Any, b Any) Any {
+	return a.(int) > b.(int)
+}
+
+func CompareGtE(a Any, b Any) Any {
+	return a.(int) >= b.(int)
+}
+
+func CompareEq(a Any, b Any) Any {
+	return a.(int) == b.(int)
+}
+
+func CompareNotEq(a Any, b Any) Any {
+	return a.(int) != b.(int)
+}
+
+func BinOpAdd(a Any, b Any) Any {
+	return a.(int) + b.(int)
+}
+
+func BinOpSub(a Any, b Any) Any {
+	return a.(int) - b.(int)
+}
+
+func BinOpMult(a Any, b Any) Any {
+	return a.(int) * b.(int)
+}
+
+func BinOpDiv(a Any, b Any) Any {
+	return a.(int) / b.(int)
+}
+
+func BinOpMod(a Any, b Any) Any {
+	return a.(int) % b.(int)
+}
