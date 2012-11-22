d00c7629ae1b9657d121b5aa6c43a6d94c56f978
central <central@think.yak.net>
2012-11-22 00:53:00 -0800

    t5.py

---
+++ t5.py
@@ -0,0 +1,31 @@
+# Simple 'Hello World' web server.
+
+import os
+import fmt
+from net import http
+
+def home(w, r):
+  fmt.Fprintf(w, 'Hello World!')
+
+plot = os.Args[1]
+http.HandleFunc('/', home)
+http.ListenAndServe('127.0.0.1:' + port, None)
+
+# Based on this golang code:
+##  package main
+##  
+##  import (
+##    "os"
+##    "fmt"
+##    "net/http"
+##  )
+##  
+##  func home(w http.ResponseWriter, r *http.Request) {
+##    fmt.Fprintf(w, "Hello World!")
+##  }
+##  
+##  func main() {
+##    port := os.Args[1]
+##    http.HandleFunc("/", home)
+##    http.ListenAndServe("127.0.0.1:" + port, nil)
+##  }
